1f7b06708a9535d7dc1525ae47150a0397a6d703
Joel Delatte <joeldelatte@gmail.com>
2019-11-16 01:18:19 -0600

    adds further functionality and styling. Needs local storage of searched for cities and 5 day forecast, and icons

--- script.js
+++ script.js
@@ -1,44 +1,82 @@
+var moment = moment()
 var apiKey = "947259a50f48f4610e089efe46fccce6";
-// var city = $("#city-search").val().toLowerCase()
-var city = "London"
 
-var queryURL =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+var temperatureConverter = function(valNum) {
+    valNum = parseFloat(valNum);
+    var Farenheit = ((valNum-273.15)*1.8)+32
+    return Farenheit.toFixed();
+} 
 
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
+var loadWeather = function() {
+    var x = $("#city-search").val();
+    var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${x}&appid=${apiKey}`
     
-    var temperature = response.main.temp;
-    var windSpeed = response.wind.speed;
-    var cityName = response.name
-    var time = response
-    console.log(windSpeed, cityName, time)
-    function temperatureConverter(valNum) {
-        valNum = parseFloat(valNum);
-        // document.getElementById("outputFahrenheit").innerHTML=((valNum-273.15)*1.8)+32;
-        var Farenheit = ((valNum-273.15)*1.8)+32
-        console.log(Farenheit.toFixed(2))
-    } 
-    var longitude = response.coord.lon
-    var lattitude = response.coord.lat
-    temperatureConverter(temperature)
-    var uvIndex = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lattitude}&lon=${longitude}`;
     $.ajax({
-        url: uvIndex,
+        url: queryURL,
         method: "GET"
-    }).then(function(response) {
-        console.log(response.value);
-    })
-}); 
+    }).then(function(response){
+        console.log(response);
+        var icon = response.weather[0].icon;
+        var weatherIcon = "<img src='" + icon  + "'>";
+        var temperature = response.main.temp;
+        var windSpeed = response.wind.speed;
+        var cityName = response.name;
+        var date = moment.format("M/D/YYYY")
+        var humid = response.main.humidity;
+        var longitude = response.coord.lon
+        var lattitude = response.coord.lat
+        var uvIndex = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lattitude}&lon=${longitude}`;
+        // function to convert temp from Kelvin to Farenheit
+        
+        $("#temp-faren").empty();
+        $("#temp-faren").append(`Temperature: ${temperatureConverter(temperature)}° F.`);
+        $("#date-time-icon").empty();
+        $("#date-time-icon").append(`${cityName} (${date}) ${weatherIcon}`);
+        ;
+        $("#humidity").empty();
+        $("#humidity").append(`Humidity: ${humid}%`);
+        $("#wind-speed").empty();
+        $("#wind-speed").append(`Wind-speed: ${windSpeed}`);
+    
+        var uvIndex = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lattitude}&lon=${longitude}`;
+        $.ajax({
+            url: uvIndex,
+            method: "GET"
+        }).then(function(response) {
+            $("#uv-index").text(`UV Index: ${response.value}`);
+        });
+        
+        var forecast = `http://api.openweathermap.org/data/2.5/forecast?q=${x}&appid=${apiKey}`
+        $.ajax({
+            url: forecast,
+            method: "GET"
+        }).then(function(response) {
+            $("#date-1").empty();
+            $("#date-1").append(moment.format("M/D/YYYY"));
+            $()
+            $("#temp-1").append(`Temp: ${temperatureConverter(response.list[0].main.temp)}°`);
+            $("#humidity-1").empty();
+            $("#humidity-1").append(`Humidity: ${response.list[0].main.humidity}%`);
+            console.log(response.list[0])
 
-var forecast = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
-$.ajax({
-    url: forecast,
-    method: "GET"
-}).then(function(response) {
-    // for loop here to iterate through the forecast arrays ?
-    console.log(response.list[i])
-})
+            //  
+        })
+
+
+    })};
+
+$("#button-search").on("click", function() {
+    event.preventDefault();
+    x = $("#city-search").val();
+    $("#search-history").prepend(
+        `<section class="list-group-item list-group-item-action clicked">${x}</section>`
+    );
+    loadWeather();
+  })
+
+// loadWeather();
+
+ 
+
+
+
